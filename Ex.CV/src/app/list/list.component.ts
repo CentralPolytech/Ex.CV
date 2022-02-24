147131c193442937ed7c69c8c895111d36bf3539
Haythem Saad <h.saad1905@myu.universitecentrale.tn>
2022-02-25 00:26:27 +0100

    Ajout des directives structurelles

--- Ex.CV/src/app/list/list.component.ts
+++ Ex.CV/src/app/list/list.component.ts
@@ -14,7 +14,7 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this.cvs = [
       new Cv(1, '1234', 'Haythem', 'Saad', 'Etudiant', 'rotating_card_profile3.png', 35),
-      new Cv(2, '4321', 'Takwa', 'Saad', 'Cadre Administratif', 'rotating_card_profile.png', 25),
+      new Cv(2, '4321', 'Takwa', 'Saad', 'Enseignante', 'rotating_card_profile.png', 25),
       new Cv(3, '5678', 'Bilel', 'Saad', 'Ingénieur', 'rotating_card_profile2.png', 34),
     ]
   }
